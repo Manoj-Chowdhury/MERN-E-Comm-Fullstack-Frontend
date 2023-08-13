ce6749a91d3ba6b997e753f07fca109834455d4a
Manoj Chowdhury <manojchowdhury599@gmail.com>
2023-08-13 14:00:33 +0530

    part2 : login & signup done with proper routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import ProductList from "./features/product-list/ProductList";
 import Home from "./pages/Home";
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      {/* <ProductList /> */}
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
-
 export default App;
